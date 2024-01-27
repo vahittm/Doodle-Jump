a022d27f7843176b7719a8bb1501fbb1d6a8ed58
vahittm <154670042+vahittm@users.noreply.github.com>
2024-01-27 11:44:46 +0330

    Enemy2 and Hole Destroy method added

--- Assets/Scripts/Enemy2.cs
+++ Assets/Scripts/Enemy2.cs
@@ -3,12 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
-{ 
+public class Enemy2 : MonoBehaviour
+{
     public float speed = 2.0f;
-    public Vector3 pointA = new Vector3(-1.3f, 0, 0);
+
+    public Vector3 pointA = new Vector3 (-1.2f,0,0);
+
     public Vector3 pointB = new Vector3(0, 0, 0);
-    public int direction = 1;
+
+    private float direction = 1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,11 +23,11 @@ public class Enemy : MonoBehaviour
     {
         transform.position += Vector3.right * (speed * direction * Time.deltaTime);
 
-        if (transform.position.x<= pointA.x)
+        if (transform.position.x<=pointA.x)
         {
             direction = 1;
         }
-       else if (transform.position.x>= pointB.x)
+        else if(transform.position.x>=pointB.x)
         {
             direction = -1;
         }
@@ -32,15 +35,15 @@ public class Enemy : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag=="Player")
+        if (other.tag == "Player")
         {
             Destroy(other.gameObject);
         }
 
-        if (other.tag=="Bullet")
+        if (other.tag == "Bullet")
         {
-            Destroy(this.gameObject);
             Destroy(other.gameObject);
+            Destroy(this.gameObject);
         }
     }
 }
